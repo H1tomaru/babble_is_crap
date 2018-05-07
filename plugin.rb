525aac419ca36f5aaf7bf07130981d1cf8da6a35
H1tomaru <watashiwa@inbox.ru>
2018-05-07 17:39:59 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -17,20 +17,20 @@ after_initialize do
 				params = {
 					encodedChatId: '322jN',
 					siteDomain: 'union3.ru',
-					siteUserExternalId: current_user[:id],
-					siteUserFullName: current_user[:username],
-					siteUserAvatarUrl: current_user[:avatar_template].sub!('{size}', '120'),
-					siteUserProfileUrl: "/u/"+current_user[:username]
+					siteUserExternalId: current_user.id,
+					siteUserFullName: current_user.username,
+					siteUserAvatarUrl: current_user.avatar_template.sub!('{size}', '120'),
+					siteUserProfileUrl: "/u/"+current_user.username
 				}
 				params[:signature] = Digest::MD5.hexdigest(
-					params[:siteDomain] +
-					params[:siteUserExternalId] +
-					params[:siteUserFullName] +
-					params[:siteUserAvatarUrl] +
-					params[:siteUserProfileUrl] +
+					params.siteDomain +
+					params.siteUserExternalId +
+					params.siteUserFullName +
+					params.siteUserAvatarUrl +
+					params.siteUserProfileUrl +
 					'291c5e1b-c0a3-49ec-a38c-c180a54fe4a2'
 				)
-				render json: { params: params }
+				render json: { params: params, current_user: current_user }
 			end
 		end
 
