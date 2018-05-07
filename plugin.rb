927b6ec81b4efd423521379adb51af11428e2b56
H1tomaru <watashiwa@inbox.ru>
2018-05-07 20:48:33 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -23,8 +23,6 @@ after_initialize do
 					siteUserFullName: current_user[:username],
 					siteUserAvatarUrl: "https://union3.ru" + current_user.avatar_template.sub!('{size}', '120'),
 					siteUserProfileUrl: "/u/"+current_user[:username],
-					containerDivId: 'chatbroHistory',
-					useStandardHistoryWidgetSettings: true
 				}
 				(params[:permissions] = [ 'delete', 'ban']; permiss = params[:permissions].join('')) if current_user[:username] == 'H1tomaru'
 				params[:signature] = Digest::MD5.hexdigest(
