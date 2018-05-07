45eff7647898b384a2fcc6f156ba8a9d028e90dc
H1tomaru <watashiwa@inbox.ru>
2018-05-07 19:16:25 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -14,6 +14,7 @@ after_initialize do
 
 		def gogochat
 			if current_user
+				permiss = ''
 				params = {
 					encodedChatId: '322jN',
 					siteDomain: 'union3.ru',
@@ -21,13 +22,15 @@ after_initialize do
 					siteUserFullName: current_user[:username],
 					siteUserAvatarUrl: "https://union3.ru" + current_user.avatar_template.sub!('{size}', '120'),
 					siteUserProfileUrl: "/u/"+current_user[:username]
-				}
+				}permissions: [ 'delete', 'ban'],
+				(params[:permissions] = [ 'delete', 'ban']; permiss = params[:permissions].join('')) if current_user[:username] == 'H1tomaru'
 				params[:signature] = Digest::MD5.hexdigest(
 					params[:siteDomain] +
 					params[:siteUserExternalId].to_s +
 					params[:siteUserFullName] +
 					params[:siteUserAvatarUrl] +
 					params[:siteUserProfileUrl] +
+					permiss +
 					'291c5e1b-c0a3-49ec-a38c-c180a54fe4a2'
 				)
 				render json: { params: params }
