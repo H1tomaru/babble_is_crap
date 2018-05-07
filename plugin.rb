6e3544e8bde02f163e232f583785d8b579802494
H1tomaru <watashiwa@inbox.ru>
2018-05-07 18:35:21 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -19,7 +19,7 @@ after_initialize do
 					siteDomain: 'union3.ru',
 					siteUserExternalId: current_user[:id],
 					siteUserFullName: current_user[:username],
-					siteUserAvatarUrl: current_user.avatar_template.sub!('{size}', '120'),
+					siteUserAvatarUrl: "https://union3.ru" + current_user.avatar_template.sub!('{size}', '120'),
 					siteUserProfileUrl: "/u/"+current_user[:username]
 				}
 				params[:signature] = Digest::MD5.hexdigest(
