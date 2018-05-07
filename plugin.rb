69ef8524ac1fc3d415bc97a79ca10ebc35f7d87b
H1tomaru <watashiwa@inbox.ru>
2018-05-07 14:17:54 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,19 @@
-123
+# name: MrBug-ChatBro
+# version: 9.9.9
+# authors: MrBug
+
+after_initialize do
+
+	Discourse::Application.routes.append do
+			get '/chatbroauth' => 'chatbro#gogochat'
+	end
+
+	class ::ChatbroController < ::ApplicationController
+
+		def gogochat
+			render json: { testing: true }
+		end
+
+	end
+
+end
