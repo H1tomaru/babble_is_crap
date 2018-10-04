ee2565d7e0a0ab5ad74a49437b259c5c613a3e9d
H1tomaru <watashiwa@inbox.ru>
2018-10-04 17:57:01 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -24,7 +24,8 @@ after_initialize do
 					siteUserAvatarUrl: "https://union3.ru" + current_user.avatar_template.sub!('{size}', '120'),
 					siteUserProfileUrl: "/u/"+current_user[:username],
 				}
-				(params[:permissions] = [ 'delete', 'ban']; permiss = params[:permissions].join('')) if ['H1tomaru', 'D13SELBOY'].include?(current_user[:username])
+				(params[:permissions] = [ 'delete', 'ban']; permiss = params[:permissions].join('')) if ['H1tomaru','D13SELBOY', 'Bodwaizer'].include?(current_user[:username])
+				#(params[:permissions] = [ 'delete']; permiss = params[:permissions].join('')) if ['D13SELBOY', 'Bodwaizer'].include?(current_user[:username])
 				params[:signature] = Digest::MD5.hexdigest(
 					params[:siteDomain] +
 					params[:siteUserExternalId].to_s +
